a971d75cfed983da22ed4c389e2fa87dee52f58f
jay <jay@pop-os.localdomain>
2022-09-12 22:12:05 +0200

    adding some updates from beginning of part2

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -4,33 +4,36 @@
 
     <input type="email" name="email" v-model="email" placeholder="email" />
     <br />
-    <input type="password" name="password" v-model="password" placeholder="password" />
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
     <br />
     <button @click="register">Register</button>
   </div>
 </template>
 
 <script>
-
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from "@/services/AuthenticationService";
 export default {
-  data () {
+  data() {
     return {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   },
   methods: {
-    async register () {
+    async register() {
       const response = await AuthenticationService.register({
         email: this.email,
         password: this.password
-      })
-      console.log(response.data)
+      });
+      console.log(response.data);
     }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
